8f55d7c6662a349a9e72c4110ebec034a2b5bd12
Andrea Gotelli <andrea.gotelli@utoronto.ca>
2026-09-13 18:20:21 -0400

    generated data for all trajectories

--- revised/figshare_revised/code/postprocessing/process_data.m
+++ revised/figshare_revised/code/postprocessing/process_data.m
@@ -8,7 +8,7 @@ addpath("tests\")
 
 %% ====== PATHS / SETTINGS ======
 data_root = fullfile("../../", "data/");
-folder = fullfile(data_root, "dynamic_motion/","star_fast/");
+folder = fullfile(data_root, "quasi_static/","static_bend_y_n200/");
 
 
 %%  Postprocessing properties
